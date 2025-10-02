713143056862e8bd2de1ecd4e2ff3b8c053dd5a2
CodenTheSynth <watemborskae@gmail.com>
2025-10-02 16:39:11 +0200

    Steam Blower now has Updated Graphics

--- Clonkworks.c4f/Clonkworks.c4d/Vehicles.c4d/Steam-Blower.c4d/Script.c
+++ Clonkworks.c4f/Clonkworks.c4d/Vehicles.c4d/Steam-Blower.c4d/Script.c
@@ -11,16 +11,16 @@ func Initialize() {
 
 public func Fire(bool fAuto)
 {
-  var iX = ((GetPhase()+GetPhase()/2)-5)*(GetDir()*2-1);
-  var iY = (GetPhase()*2)-25;
+  var iX = ((GetPhase()+GetPhase()/2)-8)*(GetDir()*2-1);
+  var iY = (GetPhase()*2)-40;
   var iAngle = BoundBy(GetPhase()*5-6,0,90);
   var iXDir = Sin(iAngle,32*(GetDir()*2-1));
   var iYDir = Cos(iAngle,-20)+GetPhase()-6;
 	
-	CreateParticle("PSpark",iXDir,iYDir,0,0,250,RGBa(255,0,0));
-	CreateParticle("PSpark",iXDir*2,iYDir*2,0,0,200,RGBa(255,0,0));
-	CreateParticle("PSpark",(iXDir*3),(iYDir*3),0,0,180,RGBa(255,0,0));
-	CreateParticle("PSpark",(iXDir*4),(iYDir*4),0,0,150,RGBa(255,0,0));
+	CreateParticle("PSpark",iX+iXDir,iY+iYDir,0,0,250,RGBa(255,0,0));
+	CreateParticle("PSpark",iX+iXDir*2,iY+iYDir*2,0,0,200,RGBa(255,0,0));
+	CreateParticle("PSpark",iX+(iXDir*3),iY+(iYDir*3),0,0,180,RGBa(255,0,0));
+	CreateParticle("PSpark",iX+(iXDir*4),iY+(iYDir*4),0,0,150,RGBa(255,0,0));
  CreateParticle("PSpark",iX,iY,iXDir+RandomX(-5,5),iYDir+RandomX(-5,5),75,RGBa(255,255,255));
   return(1);
 }
