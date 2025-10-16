ff8d6309b199ce25645d1588feefd7fc4a271371
CodenTheSynth <watemborskae@gmail.com>
2025-10-16 20:43:12 +0200

    Added legs for the cave crawler

--- Clonkworks.c4d/Vehicles.c4d/Cave Climber.c4d/Script.c
+++ Clonkworks.c4d/Vehicles.c4d/Cave Climber.c4d/Script.c
@@ -4,13 +4,18 @@
 
 #include DOOR
 
+local LegVerts;
+local LegList;
+
 func Initialize() {
-	SetGraphics("Player",this(),GetID(),GFX_Overlay,1);
-	SetClrModulation(RGBa(0,0,0,0),this(),1);
+	LegVerts = [5,6,7,8,9,10];
+	LegList = [];
+	SetGraphics("Window",this(),GetID(),GFX_Overlay,6);
+	SetClrModulation(RGBa(255,255,255),this(),1);
 	return(1);
 }
 
-//changing graphics
+//changing graphics & object entrance
 
 func Collection2(pObj){
 	if(!CrewMember(pObj)) return(0);
@@ -19,13 +24,15 @@ func Collection2(pObj){
 }
 
 func Ejection(){
-	SetClrModulation(RGBa(0,0,0,0),this(),1);
+	SetClrModulation(RGBa(255,255,255),this(),1);
 	SetGraphics(0,this());
 }
 
-func RejectEntrance(pByObj){
-	if(CrewMember(pByObj)) return(0);
-	return(1);
+func ActivateEntrance(pByObj){
+	//Log(GetCommand(pByObj));
+	if(GetCommand(pByObj,0) == "Exit") return(_inherited(pByObj));
+	if(CrewMember(pByObj) && FindObject2(Find_OCF(OCF_CrewMember), Find_Container(this()))) return(0);
+	return(_inherited(pByObj));
 }
 
 //control
@@ -35,6 +42,8 @@ public func ContainedUp(pByObj){
 	if(GetBGWall(0,0) && GetAction() == "Idle"){
 		Sound("Click");
 		SetAction("Movement");
+		DeployLegs();
+		SetComDir(COMD_Stop);
 		return(1);
 	}
 	
@@ -97,6 +106,7 @@ public func ContainedDownDouble(pByObj){
 	if(GetAction() != "Movement") return(0);
 	Sound("Click");
 	SetAction("Idle");
+	UndeployLegs();
 }
 
 //jump'n'run controls
@@ -109,9 +119,6 @@ public func ContainedUpdate(object self, int comdir, bool dig, bool throw)
   if(GetAction() != "Movement"){
 	  SetComDir(COMD_Stop);
 	  return(0);
-  }
-  if(comdir == 0){
-	  SetXDir(0); SetYDir(0);
   }
 
   return(1);
@@ -165,14 +172,76 @@ func SoundCloseDoorScheduled(sound){
 //PhaseCall
 
 protected func MoveLegs(){
-	if(!GetBGWall(0,0)) ContainedDownDouble(this());
+	//check back
+	//if(!GetBGWall(0,0)) ContainedDownDouble(this());
+
+	//check legs attached to wall. if 0, fall.
+	var attachedAmount = 0;
+	for(var lg in LegList){
+		attachedAmount += lg->IsAttachedToWall();
+	}
+	if(attachedAmount <= 3 && GetActTime() > 2) ContainedDownDouble(this());
 	
-	//bugged
-/* 	
-	if(GBackSolidAtCircle(GetX(),GetY(),20)){
-	DigFree(GetX(),GetY(),20);
-	Sound("Dig*");
-	} */
+	//fixing velocity
+	if(GetComDir() == COMD_Stop){
+		SetXDir(0); SetYDir(0);
+	}
+	
+	if(GetComDir() == COMD_Up || GetComDir() == COMD_Down){
+		SetXDir(0);
+	}
+	
+	if(GetComDir() == COMD_Left || GetComDir() == COMD_Right){
+		SetYDir(0);
+	}
+	
+	//move legs
+	for(var i = 0; i < GetLength(LegList); i++){
+		var leg = LegList[i];
+		leg->SetR(Angle(GetX(leg),GetY(leg),GetX(), GetY())-90);
+		var dist = Distance(GetX(leg)+GetVertex(1,0,leg), GetY(leg)+GetVertex(1,1,leg), GetX()+GetVertex(i,0), GetY()+GetVertex(i,1));
+		if(dist > 30 && attachedAmount >= 3){
+			if(GetComDir() == COMD_Stop) continue;
+			if(leg->GetAction() == "MoveLeg") continue;
+			attachedAmount -= 1;
+			var oX, oY;
+			oX = GetX()-GetX(leg);
+			
+			if(GetComDir() == COMD_Left || GetComDir() == COMD_UpLeft || GetComDir() == COMD_DownLeft) oX -= 40;
+			if(GetComDir() == COMD_Right || GetComDir() == COMD_UpRight || GetComDir() == COMD_DownRight) oX += 40;
+			
+			oY = GetY()-GetY(leg);
+			
+			if(GetComDir() == COMD_Up || GetComDir() == COMD_UpLeft || GetComDir() == COMD_UpRight) oY -= 40;
+			if(GetComDir() == COMD_Down || GetComDir() == COMD_DownLeft || GetComDir() == COMD_DownRight) oY += 40;
+			
+			leg->MoveToPlace(oX+GetVertex(i,0),oY+GetVertex(i,1));
+		}
+	}
+}
+
+//deployment and unemployment of legs
+protected func DeployLegs(){
+	//lets avoid duplicates
+	for(var leg in LegList){
+		RemoveObject(leg);
+	}
+	
+	for(var i in LegVerts){
+		var newLeg = CreateObject(CCLG, GetVertex(i,0),GetVertex(i,1));
+		LegList[GetIndexOf(i,LegVerts)] = newLeg;
+	}
+}
+
+protected func UndeployLegs(){
+	for(var leg in LegList){
+		RemoveObject(leg);
+	}
+	LegList = [];
+}
+
+private func legrem(rem){
+	RemoveObject(rem);
 }
 
 // Reject content menu
---
+++ Clonkworks.c4d/Vehicles.c4d/Cave Climber.c4d/leg.c4d/Script.c
@@ -0,0 +1,68 @@
+/*-- Neues Script --*/
+
+#strict
+
+local tX;
+local tY;
+local ttX;
+local ttY;
+local NoCol;
+
+func Initialize() {
+  
+  return(1);
+}
+
+public func MoveToPlace(iX,iY){
+	tX = iX;
+	tY = iY;
+	ttX = GetX()+tX;
+	ttY = GetY()+tY;
+	
+	var XDif = AbsX(GetX()+tX);
+	XDif/=16;
+	var YDif = AbsY(GetY()+tY);
+	YDif/=16;
+	
+	tX = XDif;
+	tY = YDif;
+	
+	SetAction("MoveLeg");
+	if(Distance(GetX(),GetY(),GetX()+iX,GetY()+iY) > 100) NoCol = true;
+	else NoCol = false;
+	AddEffect("Legmove",this(),100,1,this());
+}
+
+protected func FxLegmoveTimer(object pTarget, int iEffectNumber, int iEffectTime){
+	if(iEffectTime > 16) return(-1);
+	if(!NoCol && iEffectTime > 1)
+	if(GBackSolid(tX,tY) || GBackSolid(GetVertex(0,0), GetVertex(0,1)) || !GetBGWall(tX,tY)) return(-1);;
+	SetPosition(GetX()+tX,GetY()+tY,,16);
+}
+
+protected func FxLegmoveStop(){
+	SetAction("Idle");
+}
+
+public func IsAttachedToWall(){
+	if(GetAction() == "MoveLeg"){
+		return(0);
+	}
+	return(GetBGWall(GetVertex(0,0),GetVertex(0,1)) || GBackSolid(GetVertex(0,0), GetVertex(0,1)));
+}
+
+protected func Clonk(){
+		var Mat = GetMaterial(GetVertex(0,0),GetVertex(0,1));
+		var col1,col2;
+		col1 = RGBa(GetMaterialVal("Color", "Material", Mat,0),GetMaterialVal("Color", "Material", Mat,1),GetMaterialVal("Color", "Material", Mat,2));
+		if(GetMaterialVal("Color", "Material", Mat,3)) col2 = RGBa(GetMaterialVal("Color", "Material", Mat,3),GetMaterialVal("Color", "Material", Mat,4),GetMaterialVal("Color", "Material", Mat,5));
+		else col2 = col1;
+		
+		CastParticles("PxSpark",RandomX(3,9),25,GetVertex(0,0),GetVertex(0,1),10,40,col1,col2);
+		
+		Sound("ClimbHit*");
+}
+
+/* protected func FxLegmoveEnd(object pTarget, int iEffectNumber, int iEffectTime){
+	SetPosition(ttX,ttY,,16);
+} */
