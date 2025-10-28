77c05619f8d61ec0e6e3e7db70e1869b787db14f
CodenTheSynth <watemborskae@gmail.com>
2025-10-28 16:53:53 +0100

    Syringe is now fully functional

--- Clonkworks.c4d/Items.c4d/Consumeable.c4d/Syringe.c4d/Script.c
+++ Clonkworks.c4d/Items.c4d/Consumeable.c4d/Syringe.c4d/Script.c
@@ -5,6 +5,7 @@ local Essence;
 
 func Initialize() {
   Update();
+  AddEffect("Syrinf",this(),1,0,this());
   return(1);
 }
 
@@ -16,11 +17,13 @@ func Update(){
 	}
 }
 
-public func Activate(){
+public func Activate(pClonk){
 	[$TxtInject$]
 	if(!Essence) SetAction("Idle");
 	var NotFilled = true;
 	if(GetAction() == "Filled") NotFilled = false;
+	
+	if(GetEffect("Injection",pClonk)) return(1);
 	
 	if(NotFilled){
 	var Injectable = FindObject2(Find_Func("HasEssence"), Find_AtPoint(), Find_NoContainer());
@@ -30,12 +33,53 @@ public func Activate(){
 		return(1);
 	}
 	
-	//TODO: replace with effect
-	Inject();
+	AddEffect("Injection",pClonk,300,1,this());
+	}
+	
+	if(!NotFilled){
+		if(GetAction(pClonk) != "Walk") {
+			Message("$ErrBadPos$",Contained(),GetName(Contained()));
+			Contained()->Sound("CommandFailure1");
+			return(1);
+		}
+		
+		if(GetEffect("Injected",pClonk)){
+			Message("$ErrAlreadyUsed$",Contained());
+			Contained()->Sound("CommandFailure1");
+			return(1);
+		}
+		
+		AddEffect("Injection",pClonk,300,1,this());
 	}
 	
 	return(1);
 }
+
+//Injection effect! comes with cool animation :D
+protected func FxInjectionStart(pTarget,iEffectNumber,iTemp){
+	//make the clonk kneel down to inject.
+	if(GetActMapVal("Name","KneelDown",GetID(pTarget)))
+	pTarget->SetAction("KneelDown");
+}
+
+protected func FxInjectionTimer(pTarget,iEffectNumber,iEffectTime){
+	//not kneeling? lets see how to end this...
+	if(GetAction(pTarget) != "KneelDown"){
+		//if the target has a kneeling animation
+		if(GetActMapVal("Name","KneelDown",GetID(pTarget)) && GetActMapVal("Name","KneelUp",GetID(pTarget))){
+			if(GetAction(pTarget) == "KneelUp") EffectVar(0,pTarget,iEffectNumber) = 1; //ok, can do injection!
+			return(-1);
+		}else{
+			if(GetAction(pTarget) == "Walk" || GetAction(pTarget) == "Idle") EffectVar(0,pTarget,iEffectNumber) = 1; //ok, can do injection!
+			return(-1);
+		}
+	}
+}
+
+protected func FxInjectionStop(pTarget,iEffectNumber,iReason,fTemp){
+	if(EffectVar(0,pTarget,iEffectNumber) == 1) Inject();
+}
+
 
 protected func Inject(){
 	var NotFilled = true;
@@ -51,15 +95,42 @@ public func DoExtraction(){
 	
 	SetColorDw(Injectable->~EssenceInfo(1)); //get and set color
 	Essence = GetID(Injectable);
-	Sound("Corrode");
+	Sound("StabExtract");
 	SetAction("Filled");
 }
 
 public func DoInjection(){
-	
+	if(!Contained() || !Essence) return(0);
+	AddEffect("Injected",Contained(),100,1,,SRIG,Essence);
+	Sound("StabInject");
+	RemoveObject();
 }
 
 //other
 func Hit(){
 	if(!Random(3)) Sound("Crystal*",0,this(),25);
+}
+
+//oooooooh! a syring effect!
+public func FxInjectedStart(pTarget,iEffectNumber, iTemp, EssenceType){
+	EffectVar(0,pTarget,iEffectNumber) = EssenceType;
+}
+
+public func FxInjectedTimer(pTarget, iEffectNumber, iEffectTime){
+	if(iEffectTime > 23*36) return(-1);
+	if(!Random(15) && iEffectTime > 3*36) CreateParticle("PSpark",GetX(pTarget)+RandomX(-8,8),GetY(pTarget)+8,0,-5,RandomX(25,40),EffectVar(0,pTarget,iEffectNumber)->~EssenceInfo(1),pTarget);
+	if(iEffectTime != 3*36) return(0);
+	var EssenceType = EffectVar(0,pTarget,iEffectNumber);
+	DefinitionCall(EssenceType,"EssenceInfo",2,pTarget);
+}
+
+public func FxInjectedStop(pTarget,iEffectNumber, iTemp){
+	var EssenceType = EffectVar(0,pTarget,iEffectNumber);
+	DefinitionCall(EssenceType,"EssenceInfo",3,pTarget);
+}
+
+//oooh! info!
+public func FxSyrinfInfo(object pTarget, int iEffectNumber){
+	var i = Format("$TxtEssence$",Essence,GetName(,Essence));
+	if(Essence) return(i);
 }
